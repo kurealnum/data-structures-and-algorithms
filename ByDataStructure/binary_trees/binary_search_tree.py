25185892cb116b56ed985f1d8902df80afb9f097
kurealnum <cattledoger@gmail.com>
2023-07-07 13:52:15 -0400

    started getting multiple binary tree files set up

---
+++ ByDataStructure/binary_trees/binary_search_tree.py
@@ -0,0 +1,110 @@
+from binary_tree import node, binary_tree
+
+#inherits all properties from node
+class node(node):
+    pass
+
+
+
+#binary search trees are pretty much just binary trees, thus its smoooth inheritance
+class binary_search_tree(binary_tree):
+    
+    #TBD stands for To Be Deleted, takes tree root and new node *value* as input
+    def delete_node(self, root, k):
+        #base case
+        if root is None:
+            return root
+    
+        #recursive calls for ancestors of
+        #node to be deleted
+        if root.data > k:
+            root.left = self.delete_node(root.left, k)
+            return root
+        elif root.data < k:
+            root.right = self.delete_node(root.right, k)
+            return root
+    
+        #we reach here when root is the node to be deleted.
+    
+        #if one of the children is empty
+        if root.left is None:
+            temp = root.right
+            del root
+            return temp
+        elif root.right is None:
+            temp = root.left
+            del root
+            return temp
+    
+        #if both children exist
+        else:
+            succParent = root
+    
+            #find successor
+            succ = root.right
+            while succ.left is not None:
+                succParent = succ
+                succ = succ.left
+    
+            #Delete successor. Since successor is always left child of its parent 
+            #we can safely make successor's right right child as left of its parent. If 
+            #there is no succ, then assign succ.right to succParent.right
+            if succParent != root:
+                succParent.left = succ.right
+            else:
+                succParent.right = succ.right
+    
+            #copy Successor Data to root
+            root.data = succ.data
+    
+            #delete Successor and return root
+            del succ
+            return root
+
+
+    #takes the root and a new node() object as input
+    def insert(self, root, node):
+        #if the root is none, just set the node to the root
+
+        #maybe check the actual root, not the arg
+        if self.root is None:
+            self.root = node
+
+        #if it isn't (it won't be most times of course)
+        else:
+            #if the root is smaller than the node, we go down the right side
+            if root.data < node.data:
+                #if there's nothing in the right node, put the node there
+                if root.right is None:
+                    root.right = node
+                    self.node_count += 1
+                
+                #if there is, go ahead and recursively call the function again,
+                #this time having the root set as the right node
+                else: 
+                    self.insert(root.right, node)
+
+            #if the root is bigger than the node, we go down the left side
+            elif root.data > node.data:
+                #if there's nothing in the left node, put the node there
+                if root.left is None:
+                    root.left = node
+                    self.node_count += 1
+
+                #if there is, recursively call the function again with the "root"
+                #argument set as the left node that we're currently on
+                else: 
+                    self.insert(root.left, node)
+
+            #if the root is equal to the current node
+            else:
+                #worst comes to worst, we just put it in the left node
+                if root.left is None:
+                    root.left = node
+                    self.node_count += 1
+
+                #if there is something in the left node, go check that out
+                else:
+                    self.insert(root.left, node)
+
+
--- ByDataStructure/binary_trees/binary_tree.py
+++ ByDataStructure/binary_trees/binary_tree.py
@@ -1,3 +1,8 @@
+#----------------------------------------------------------------------
+#This is the base file for binary trees. I wouldn't reccommend using it 
+#barebones, at least use something like binary_search_tree.py
+#----------------------------------------------------------------------
+
 import queues.queue as q
 from collections import defaultdict
 
@@ -12,7 +17,7 @@ class node:
 
 
         
-class binarytree:
+class binary_tree:
 
     def __init__(self, root=None) -> None:
         self.root = root
@@ -28,52 +33,6 @@ class binarytree:
         #loop through the list
         for i in list:
             self.insert(root, node(i))
-
-
-    #takes the root and a new node() object as input
-    def insert(self, root, node):
-        #if the root is none, just set the node to the root
-
-        #maybe check the actual root, not the arg
-        if self.root is None:
-            self.root = node
-
-        #if it isn't (it won't be most times of course)
-        else:
-            #if the root is smaller than the node, we go down the right side
-            if root.data < node.data:
-                #if there's nothing in the right node, put the node there
-                if root.right is None:
-                    root.right = node
-                    self.node_count += 1
-                
-                #if there is, go ahead and recursively call the function again,
-                #this time having the root set as the right node
-                else: 
-                    self.insert(root.right, node)
-
-            #if the root is bigger than the node, we go down the left side
-            elif root.data > node.data:
-                #if there's nothing in the left node, put the node there
-                if root.left is None:
-                    root.left = node
-                    self.node_count += 1
-
-                #if there is, recursively call the function again with the "root"
-                #argument set as the left node that we're currently on
-                else: 
-                    self.insert(root.left, node)
-
-            #if the root is equal to the current node
-            else:
-                #worst comes to worst, we just put it in the left node
-                if root.left is None:
-                    root.left = node
-                    self.node_count += 1
-
-                #if there is something in the left node, go check that out
-                else:
-                    self.insert(root.left, node)
     
 
     def print_tree(self):
@@ -165,59 +124,6 @@ class binarytree:
             self.in_order_traversal(root.right,visited)
 
         return visited
-
-
-    #TBD stands for To Be Deleted, takes tree root and new node *value* as input
-    def delete_node(self, root, k):
-        #base case
-        if root is None:
-            return root
-    
-        #recursive calls for ancestors of
-        #node to be deleted
-        if root.data > k:
-            root.left = self.delete_node(root.left, k)
-            return root
-        elif root.data < k:
-            root.right = self.delete_node(root.right, k)
-            return root
-    
-        #we reach here when root is the node to be deleted.
-    
-        #if one of the children is empty
-        if root.left is None:
-            temp = root.right
-            del root
-            return temp
-        elif root.right is None:
-            temp = root.left
-            del root
-            return temp
-    
-        #if both children exist
-        else:
-            succParent = root
-    
-            #find successor
-            succ = root.right
-            while succ.left is not None:
-                succParent = succ
-                succ = succ.left
-    
-            #Delete successor. Since successor is always left child of its parent 
-            #we can safely make successor's right right child as left of its parent. If 
-            #there is no succ, then assign succ.right to succParent.right
-            if succParent != root:
-                succParent.left = succ.right
-            else:
-                succParent.right = succ.right
-    
-            #copy Successor Data to root
-            root.data = succ.data
-    
-            #delete Successor and return root
-            del succ
-            return root
         
 
     #takes a sorted array as input. use with caution, will partially 
@@ -252,7 +158,7 @@ if __name__ == "__main__":
     root = node(nums[mid])
     nums.pop(mid)
 
-    bt = binarytree(root)
+    bt = binary_tree(root)
     bt.fill_tree(bt.root,nums)
     #end of init stuff-----------
 
