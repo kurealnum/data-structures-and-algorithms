f6fa17aa7ab135d0e52c4d2b9b91d00291276ab9
kurealnum <cattledoger@gmail.com>
2023-07-12 15:29:02 -0400

    added formatting

--- ByDataStructure/arrays/Sorts/bucket_sort.py
+++ ByDataStructure/arrays/Sorts/bucket_sort.py
@@ -1,13 +1,18 @@
 def bucket_sort(arr):
+    #create the list of buckets
     buckets = [[] for i in arr]
     arr_len = len(arr)
 
+    #put elements in bucket
     for i in arr:
         buckets[int(arr_len*i)].append(i)
 
+    #sort all elements in buckets
     for j in range(len(arr)):
+        #change this later on
         buckets[j] = sorted(buckets[j])
 
+    #put sorted elements back in array
     k = 0
     for i in range(len(arr)):
         for j in range(len(buckets[i])):
