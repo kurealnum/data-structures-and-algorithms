25e8b52bdd18bf7223036c90185fd95efa9c411f
kurealnum <cattledoger@gmail.com>
2023-09-01 12:09:19 -0400

    Added a DFS function

--- ByDataStructure/arrays_and_matrices/dfs_matrix.py
+++ ByDataStructure/arrays_and_matrices/dfs_matrix.py
@@ -1,3 +1,19 @@
-matrix = [[0, 0, 0], [0, 0, 0], [0, 1, 0]]
+from helpers import Helpers
 
-def dfs()
+matrix = [[0, 0, 0], [0, 0, 0], [0, 1, 0]]
+# directions = [[-1, 0], [-1, 0], [0, -1], [0, 1]]
+directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+visited = set()
+
+
+def dfs(r: int, c: int):
+    if Helpers.is_valid(matrix, r, c) or (r, c) in visited:
+        return
+
+    print(matrix[r][c], r, c)
+    visited.add((r, c))
+    for dr, dc in directions:
+        dfs(r + dr, c + dc)
+
+
+dfs(0, 0)
--- ByDataStructure/arrays_and_matrices/helpers.py
+++ ByDataStructure/arrays_and_matrices/helpers.py
@@ -1,8 +1,10 @@
 class Helpers:
+    # just swapping 2 elements
     def swap(arr: list, i: int, j: int) -> None:
         temp = arr[i]
         arr[i] = arr[j]
         arr[j] = temp
 
+    # function for checking if a movement is valid in a matrix
     def is_valid(matrix: list[list], r: int, c: int):
-        pass
+        return r < 0 or c < 0 or r == len(matrix) or c == len(matrix)
