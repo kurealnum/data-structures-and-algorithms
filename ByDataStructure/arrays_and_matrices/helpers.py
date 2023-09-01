259b7d76cfe3e15ebda1c7bd09eb9f018460b36d
kurealnum <cattledoger@gmail.com>
2023-09-01 11:49:12 -0400

    Added a helpers class

---
+++ ByDataStructure/arrays_and_matrices/helpers.py
@@ -0,0 +1,8 @@
+class Helpers:
+    def swap(arr: list, i: int, j: int) -> None:
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+
+    def is_valid(matrix: list[list], r: int, c: int):
+        pass
